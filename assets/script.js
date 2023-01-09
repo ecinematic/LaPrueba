9ad587c82d382e1de751b65d48906b3a31d09878
ecinematic <ecinematic@yahoo.com>
2023-01-09 16:57:37 -0600

    added start function and answer array functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
-document.getElementById("start").addEventListener("click", function(){
-    document.getElementById("question-container").style.visibility = "visible";
+var startBtn = document.getElementById('start');
+var quizQuestion = document.getElementById('question');
+var nxQuestion = document.getElementById('next');
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    document.getElementById('next').style.display = "block";
+    document.getElementById('start').style.display = "none";
+    document.getElementById('question-container').style.display = "block";
     var timeleft = 10;
     var downloadTimer = setInterval(function(){
     document.getElementById("countdown").innerHTML = timeleft + 
@@ -11,4 +19,19 @@ document.getElementById("start").addEventListener("click", function(){
         document.getElementById("countdown").innerHTML = " is up!"
     }
     }, 1000);
-});
+    nextQuestion();
+}
+
+function nextQuestion() {
+    selectAnswer();
+}
+
+function selectAnswer() {
+    let answers = ["Houston", "San Antonio", "Dallas", "Austin"];
+    let ans = document.querySelectorAll('.ans');
+    console.log(ans);
+    for (i = 0; i < ans.length; i++) {
+        ans[i].innerHTML = answers[i];
+    }
+
+}
